d6e1e007a28802989a41ccfa104c9b66e3ca13a2
jcoitino <cjc@xpertools.com>
2017-07-27 18:18:45 -0300

    compile on RUN / DIFF
    When RUN or DIFF, compile typescript to javascript if not done previously

--- samples/domapi.ts
+++ samples/domapi.ts
@@ -1,1 +1,12 @@
 debugger;
+
+let myDiv = document.createElement("div");
+myDiv.style.position = "absolute";
+myDiv.style.top = "100px";
+myDiv.style.left = "100px";
+myDiv.style.width = "100px";
+myDiv.style.height = "100px";
+myDiv.style.background = "blue";
+
+let myParent = document.querySelector("#output") as HTMLDivElement;
+myParent.appendChild(myDiv);
