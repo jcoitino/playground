8e1c7795707e4631fecbc0d72fc2c70a94bd6e5c
jcoitino <cjc@xpertools.com>
2017-08-07 20:32:50 -0300

    Proxy sample added

--- samples/domapi.ts
+++ samples/domapi.ts
@@ -6,15 +6,18 @@ myDiv.style.left = "100px";
 myDiv.style.width = "100px";
 myDiv.style.height = "100px";
 myDiv.style.background = "blue";
+console.log("STEP 1", myDiv.outerHTML);
 
-let myParent = document.querySelector("#output");
-myParent!.appendChild(myDiv);
+let myParent = document.querySelector("#output") as HTMLElement;
+myParent.appendChild(myDiv);
+console.log("STEP 2", myParent.outerHTML);
 
 myDiv.style.top = "200px";
 myDiv.style.left = "200px";
 myDiv.style.width = "200px";
 myDiv.style.height = "200px";
 myDiv.style.background = "red";
+console.log("STEP 3", myDiv.outerHTML);
 
 myDiv.addEventListener("mouseover", () => {
     myDiv.style.opacity = "0.2";
@@ -22,6 +25,7 @@ myDiv.addEventListener("mouseover", () => {
     myDiv.style.border = "10px dashed red";
     myDiv.style.borderRadius = "50%";
     myDiv.style.cursor = "pointer";
+    console.log("ON MOUSE OVER", myDiv.outerHTML);
 });
 
 myDiv.addEventListener("mouseout", () => {
@@ -30,21 +34,24 @@ myDiv.addEventListener("mouseout", () => {
     myDiv.style.border = "none";
     myDiv.style.borderRadius = "0";
     myDiv.style.cursor = "default";
+    console.log("ON MOUSE OUT", myDiv.outerHTML);
 });
 
 myDiv.addEventListener("mousedown", (e: MouseEvent) => {
+    let oldCursor = myDiv.style.cursor;
     myDiv.style.cursor = "move";
-    let moveHandler = (e: MouseEvent) => {
+
+    let mouseMoveHandler = (e: MouseEvent) => {
         myDiv.style.top = `${myDiv.offsetTop + e.movementY}px`;
         myDiv.style.left = `${myDiv.offsetLeft + e.movementX}px`;
     };
 
-    let endHandler = () => {
-        myDiv.style.cursor = "pointer";
-        myParent!.removeEventListener("mousemove", moveHandler);
-        myParent!.removeEventListener("mouseup", endHandler);
-    }
+    let mouseUpHandler = () => {
+        myDiv.style.cursor = oldCursor;
+        myParent.removeEventListener("mousemove", mouseMoveHandler);
+        myParent.removeEventListener("mouseup", mouseUpHandler);
+    };
 
-    myParent!.addEventListener("mousemove", moveHandler);
-    myParent!.addEventListener("mouseup", endHandler);
+    myParent.addEventListener("mousemove", mouseMoveHandler);
+    myParent.addEventListener("mouseup", mouseUpHandler);
 });
---
+++ samples/proxy.ts
@@ -0,0 +1,60 @@
+let out = document.querySelector("#output") as HTMLElement;
+out.style.overflow = "auto";
+out.style.fontSize = "24px";
+let log = (m: any, ...args: any[]) => {
+    console.log(m, ...args);
+    let pre = document.createElement("pre") as HTMLElement;
+    pre.textContent = m;
+    args.forEach(a => pre.textContent += `${a}`);
+    out.appendChild(pre);
+}
+let o = {
+    a: "a string",
+    b: 123,
+    c: new Date(),
+    f: (x: number, y: number) => x + y,
+};
+let h = {
+    has: (t, p) => {
+        log(`[PROXY HANDLER] target has property '${p}'`);
+        return p in t;        
+    },
+    get: (t, p) => {
+        log(`[PROXY HANDLER] get target property '${p}'`);
+        return t[p];
+    },
+    set: (t, p, v) => {
+        log(`[PROXY HANDLER] set target property '${p}', ${0}`);
+        t[p] = v;
+        return true;
+    },
+};
+let p = new Proxy(o, h);
+
+// has
+log("a" in p);
+log("b" in p);
+log("c" in p);
+log("d" in p);
+log("f" in p);
+
+// get
+log(p.a);
+log(p.b);
+log(p.c);
+log(p.d);
+log(p.f);
+log(p.f(3, 5));
+
+// set
+p.a = "A new string";
+p.b = 456;
+p.c = new Date();
+p.d = ["a new property", "array", 789];
+p.f = (x: number, y: number) => x * y;
+log(p.a);
+log(p.b);
+log(p.c);
+log(p.d);
+log(p.f);
+log(p.f(3, 5));
